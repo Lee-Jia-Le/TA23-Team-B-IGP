ab9f68daa4fae6a827a3b0f02fe47c95ba9a133d
aLVio7 <140689610+aLVio7@users.noreply.github.com>
2023-08-08 10:31:48 +0800

    Update Overheads.py

--- Overheads.py
+++ Overheads.py
@@ -1,25 +1,41 @@
-from pathlib import Path
 import csv
+from pathlib import Path
 
-fp = Path.cwd()/"overheads-day-90.csv"
-with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-    reader = csv.reader(file)
-    next(reader)
+def process_csv_file(file_path, skip_condition=None):
+    with file_path.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header
 
-    overheadRecords=[]
+        overheadRecords = []
 
-    for row in reader:
-        overheadRecords.append([row[0], row[1]])
+        for row in reader:
+            if skip_condition and skip_condition(row):
+                continue  # Skip this row and move to the next iteration
+
+            overheadRecords.append([row[0], row[1]])  # Process the row if not skipped
+
+    return overheadRecords
+
+if __name__ == "__main__":
+    csv_file_path = Path.cwd() / "overheads-day-90.csv"
+
+    def custom_skip_condition(row):
+        # Define your custom condition here
+        # For example: return row[0] == "skip_this_row"
+        return False
+
+    result = process_csv_file(csv_file_path, skip_condition=custom_skip_condition)
+    # Now 'result' contains only the processed rows according to the condition
 
 # print(overheadRecords)
 
-for item in overheadRecords:
+for item in result:
     value = item[1]
     item[1] = value.replace("$","")
 # print(overheadRecords)
 
 Expenses_list = []
-for item in overheadRecords:
+for item in result:
     if item[1] not in Expenses_list:
         Expenses_list.append(float(item[1]))
 
@@ -31,7 +47,7 @@ def find_highest_value(list):
 
 highestValue = find_highest_value(Expenses_list)
 
-print(f"Overheadrecords: {overheadRecords}")
+print(f"Overheadrecords: {result}")
 print(f"Expense list: {Expenses_list}")
 print(f"highest value: {highestValue}")
 
@@ -40,6 +56,6 @@ print(f"highest value: {highestValue}")
 #         if it contains the highestValue, print that list
 #         else: move on
 
-for i in overheadRecords:
+for i in result:
     if float(i[1]) == highestValue:
         print(i)
