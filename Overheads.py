b2a9a44095115b28b1c76dce22c147e306a12f22
aLVio7 <140689610+aLVio7@users.noreply.github.com>
2023-08-08 14:44:43 +0800

    Update Overheads.py

--- Overheads.py
+++ Overheads.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import csv
 
-fp = Path.cwd()/"overheads-day-90.csv"
+fp = Path.cwd()/"Overheads.csv"
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
