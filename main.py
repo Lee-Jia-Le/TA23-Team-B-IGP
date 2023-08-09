8ce404cda7a5ff2b1a55286ea88f72ecf96f9735
Lee-Jia-Le <140685780+Lee-Jia-Le@users.noreply.github.com>
2023-08-09 16:33:34 +0800

    updated main.py with essential command

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import Cash_on_hand
-import Profit_and_Loss
 import Overheads
+import Cash_on_hand
+import Profit_and_loss
 
+# with open('summary_report.txt', 'w') as file:
+#     file.write(f"{print(Overheads.csv_file_path("Overheads.csv"))} \n") # prints the Highest Overheads onto the .txt file using an f string
+#     file.write(f"{print(Cash_on_hand.generate_summary_report)} \n")
+#     file.write(f"{print(Profit_and_loss.compute_decrease_days)} \n")
+#     file.write(f"{print(Profit_and_loss.compute_profit_difference)}")
