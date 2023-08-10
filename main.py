9ae497b6c411e8a7801ca2a8403f68180d832a1a
Lee-Jia-Le <140685780+Lee-Jia-Le@users.noreply.github.com>
2023-08-10 11:58:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import Cash_on_hand
 import Profit_and_loss
 
 with open('summary_report.txt', 'w') as file:
-    file.write(f"{print(Overheads.find_highest_overhead())} \n") # prints the Highest Overheads onto the .txt file using an f string
-    file.write(f"{print(Cash_on_hand)} \n")
-    file.write(f"{print(Profit_and_loss.compute_decrease_days)} \n")
-    file.write(f"{print(Profit_and_loss.compute_profit_difference)}")
+    overheads_result = Overheads.find_highest_overhead(csv_file_path='Overheads.csv')
+    file.write(f"{overheads_result}\n")
+    cash_on_hand_result = Cash_on_hand.generate_summary_report(cash_on_hand='Cash_on_Hand.csv')
+    file.write(f"{cash_on_hand_result}\n")
+    profit_surplus_result = Profit_and_loss.compute_profit_difference(data='Profits_and_Loss.')
+    file.write(f"{profit_surplus_result}\n")
+    profit_deficit_result = Profit_and_loss.compute_decrease_days(data='Profits_and_Loss.csv')
+    file.write(f"{profit_deficit_result}\n")
