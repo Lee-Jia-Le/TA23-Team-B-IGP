cf4455e4fcb20773f2cc1e89c0861fedaa043afc
Lee-Jia-Le <140685780+Lee-Jia-Le@users.noreply.github.com>
2023-08-10 10:23:54 +0800

    restructuring

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import Overheads
 import Cash_on_hand
 import Profit_and_loss
 
-# with open('summary_report.txt', 'w') as file:
-#     file.write(f"{print(Overheads.csv_file_path("Overheads.csv"))} \n") # prints the Highest Overheads onto the .txt file using an f string
-#     file.write(f"{print(Cash_on_hand.generate_summary_report)} \n")
-#     file.write(f"{print(Profit_and_loss.compute_decrease_days)} \n")
-#     file.write(f"{print(Profit_and_loss.compute_profit_difference)}")
+with open('summary_report.txt', 'w') as file:
+    file.write(f"{print(Overheads.find_highest_overhead())} \n") # prints the Highest Overheads onto the .txt file using an f string
+    file.write(f"{print(Cash_on_hand)} \n")
+    file.write(f"{print(Profit_and_loss.compute_decrease_days)} \n")
+    file.write(f"{print(Profit_and_loss.compute_profit_difference)}")
