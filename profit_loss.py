9bf44be85ec94ec6c785003a49117b5135e923a9
kuankitt <kuankitt2@gmail.com>
2023-08-10 14:38:43 +0800

    k

--- profit_loss.py
+++ profit_loss.py
@@ -1,6 +1,15 @@
 import csv
 
 def read_csv_file(file_path):
+    """
+    Reads a CSV file and returns its contents as a list of dictionaries.
+
+    Args:
+        file_path (str): Path to the CSV file.
+
+    Returns:
+        list: A list of dictionaries where each dictionary represents a row in the CSV file.
+    """
     data = []
     with open(file_path, 'r') as csv_file:
         reader = csv.DictReader(csv_file)
@@ -9,6 +18,17 @@ def read_csv_file(file_path):
     return data
 
 def compute_profit_difference(data):
+    """
+    Computes the days when the net profit increased and the day with the highest net profit increase.
+
+    Args:
+        data (list): A list of dictionaries where each dictionary represents a day's data.
+
+    Returns:
+        list: A list of day indices where net profit increased compared to the previous day.
+        int: Index of the day with the highest net profit increase.
+        float: Amount of the highest net profit increase.
+    """
     increase_days = [index for index, day_data in enumerate(data[1:], start=1) if float(day_data['Net Profit']) > float(data[index-1]['Net Profit'])]
 
     max_increment_day = None
@@ -23,6 +43,15 @@ def compute_profit_difference(data):
     return increase_days, max_increment_day, max_increment_amount
 
 def compute_decrease_days(data):
+    """
+    Computes the days when the net profit decreased and the corresponding deficit amount.
+
+    Args:
+        data (list): A list of dictionaries where each dictionary represents a day's data.
+
+    Returns:
+        list: A list of tuples where each tuple contains the day index and the deficit amount.
+    """
     decrease_days = []
     for index, day_data in enumerate(data[1:], start=1):
         net_profit = float(day_data['Net Profit'])
@@ -35,6 +64,17 @@ def compute_decrease_days(data):
     return decrease_days
 
 def get_output_strings(data):
+    """
+    Generates formatted output strings for net profit analysis.
+
+    Args:
+        data (list): A list of dictionaries where each dictionary represents a day's data.
+
+    Returns:
+        str: Formatted string indicating the days with net profit surplus.
+        str: Formatted string indicating the day with the highest net profit increase.
+        str: Formatted string indicating the days with profit deficit.
+    """
     increase_days, max_increment_day, max_increment_amount = compute_profit_difference(data)
     
     increase_days_output = ""
@@ -51,3 +91,4 @@ def get_output_strings(data):
         decrease_days_output += f"[PROFIT DEFICIT] Day: {day}, Amount: USD{int(amount)}\n"
 
     return increase_days_output, highest_increment_output, decrease_days_output
+
