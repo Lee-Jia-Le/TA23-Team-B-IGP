28d3415a781a116ecc89d002bfde2b3bcb8e3d01
Lee-Jia-Le <140685780+Lee-Jia-Le@users.noreply.github.com>
2023-08-10 15:42:17 +0800

    Update Overheads.py for Enya

--- Overheads.py
+++ Overheads.py
@@ -6,27 +6,29 @@ def find_highest_overhead(csv_file_path):
     with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
         next(reader)
-
+        #create an empty list to store the overhead records 
         overheadRecords = []
-
+        #append the first and second column of the row as a list to the overheadRecords list 
         for row in reader:
             overheadRecords.append([row[0], row[1]])
 
         for item in overheadRecords:
             value = item[1]
-            item[1] = value.replace("$", "")
-
+            item[1] = value.replace("$", "")#remove the dollar sign 
+            
+        #create an empty list to store the function 
         Expenses_list = []
         for item in overheadRecords:
             if item[1] not in Expenses_list:
-                Expenses_list.append(float(item[1]))
+                Expenses_list.append(float(item[1])) #convert the expenses to float
 
         def find_highest_value(list):
             list.sort(reverse=True)
-            return list[0]
+            return list[0] #return first element of the sorted list 
 
         highestValue = find_highest_value(Expenses_list)
-
+        
+        # creating an empty list to store overhead records with the highest value 
         highest_overheads = []
         for i in overheadRecords:
             if float(i[1]) == highestValue:
@@ -36,7 +38,7 @@ def find_highest_overhead(csv_file_path):
         formatted_result = ""
         for i in highest_overheads:
             formatted_result += "[HIGHEST OVERHEAD] " + ' : '.join(i) + "%\n"
-
+        # return the list of overhead records with the highest value 
         return formatted_result
 
 # Call the function with your CSV file path
